ebec42d7bc9f79bb94a464ef01c390aa91aa8ee5
yan203008 <255320868+yan203008@users.noreply.github.com>
2026-06-14 20:12:29 +0800

    Align record amount columns

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "yuxia-v11";
-const ASSETS = ["./", "index.html", "styles.css?v=11", "app.js?v=11", "manifest.webmanifest", "icon.svg", "icons/apple-touch-icon.png", "icons/icon-192.png", "icons/icon-512.png", "icons/icon-maskable-512.png"];
+const CACHE = "yuxia-v12";
+const ASSETS = ["./", "index.html", "styles.css?v=12", "app.js?v=12", "manifest.webmanifest", "icon.svg", "icons/apple-touch-icon.png", "icons/icon-192.png", "icons/icon-512.png", "icons/icon-maskable-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
